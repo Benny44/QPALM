805f60e55d55c905112e7c6f82ee2cff1767387d
Ben Hermans <ben.hermans2@student.kuleuven.be>
2019-03-12 23:52:08 +0100

    Make figures paper ready

--- simulations/plot_QP_comparison.m
+++ simulations/plot_QP_comparison.m
@@ -6,12 +6,16 @@ close all
 figure
 
 semilogy(n_values(1:length(Tqpalm_matlab)), Tqpalm_matlab, 'b',...
-    n_values(1:length(Tqpalm_c)), Tqpalm_c, 'm',...
     n_values(1:length(Tosqp)), Tosqp, 'r',...
     n_values(1:length(Tqpoases)), Tqpoases, 'g',...
     n_values(1:length(Tgurobi)), Tgurobi, 'k');
+%      n_values(1:length(Tqpalm_c)), Tqpalm_c, 'm',...
 grid on
-legend('QPALM (Matlab)','QPALM (C)', 'OSQP', 'qpOASES','GUROBI','Location','southeast')
+set(gca,'fontsize',14)
+xlabel('Number of primal variables')
+ylabel('Runtime (s)')
+% title(plot_title);
+legend('QPALM', 'OSQP', 'qpOASES','GUROBI','Location','southeast')
 
 end
 
