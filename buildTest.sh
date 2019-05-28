f084543f22358367a479e4e21903da1732504406
Ben Hermans <ben.hermans2@student.kuleuven.be>
2019-05-28 15:49:54 +0200

    Change metis cp command to account for extension and remove addons

--- buildTest.sh
+++ buildTest.sh
@@ -25,8 +25,7 @@ cd $metisdir
 
 cmake $curdir/suitesparse/metis-5.1.0 -DGKLIB_PATH=$curdir/suitesparse/metis-5.1.0/GKlib -DSHARED=1 && make
 cd $curdir
-ls -R
-cp build/metis/libmetis/libmetis.so build/lib/
+cp build/metis/libmetis/libmetis.* build/lib/
 
 #Build QPALM and tests
 cd $curdir
