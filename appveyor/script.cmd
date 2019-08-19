c316e5a99fd45ba9dc46c4cc056668853631295d
Ben Hermans <u0104126@kuleuven.be>
2019-08-19 11:28:57 +0200

    miniconda path

--- appveyor/script.cmd
+++ appveyor/script.cmd
@@ -1,7 +1,7 @@
 @echo on
 :: Make sure all the submodules are updated correctly
-set SUITESPARSE_ROOT_LIB=%MINICONDA_PATH%\Library\lib
-set SUITESPARSE_ROOT_INCLUDE=%MINICONDA_PATH%\include
+set SUITESPARSE_ROOT_LIB=%MINICONDA%\Library\lib
+set SUITESPARSE_ROOT_INCLUDE=%MINICONDA%\include
 
 cd %APPVEYOR_BUILD_FOLDER%
 
