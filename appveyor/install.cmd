94e49310416fd8137d5b02c6d0b6814007da323e
Ben Hermans <u0104126@kuleuven.be>
2019-08-19 14:08:22 +0200

    Fix path

--- appveyor/install.cmd
+++ appveyor/install.cmd
@@ -45,7 +45,7 @@ set SUITESPARSE_ROOT_LIB=%MINICONDA%/Library/lib
 set SUITESPARSE_ROOT_INCLUDE=%MINICONDA%/include
 
 IF "%PLATFORM%"=="x64" (
-call "C:\Program Files\Microsoft SDKs\Windows\v7.1A\Bin\SetEnv.cmd" /x64
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
 ) ELSE (
 REM Set environment for 32bit
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
