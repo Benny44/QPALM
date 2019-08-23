34ab77abc733496d457d7a57efe26c0254e34043
Ben Hermans <ben.hermans2@student.kuleuven.be>
2019-08-23 10:18:54 +0200

    Try to fix deploy syntax

--- appveyor/deploy.cmd
+++ appveyor/deploy.cmd
@@ -1,50 +1,49 @@
 @echo on
 echo %APPVEYOR_REPO_BRANCH%
-IF "%APPVEYOR_REPO_BRANCH%"=="master" (
-	:: Compile the release version
-	cd build
-	DEL /F/Q/S *.* > NUL
-	mkdir release
-	cd release
 
-	cmake ..\.. -DCMAKE_GENERATOR_PLATFORM=%PLATFORM% -DUNITTESTS=ON
-	cmake --build . --config Release
+:: Compile the release version
+cd build
+DEL /F/Q/S *.* > NUL
+mkdir release
+cd release
 
-	:: Create directories
-	set QPALM_DEPLOY_DIR="qpalm-%QPALM_VERSION%-windows64"
+cmake ..\.. -DCMAKE_GENERATOR_PLATFORM=%PLATFORM% -DUNITTESTS=ON
+cmake --build . --config Release
 
-	mkdir %QPALM_DEPLOY_DIR%
-	mkdir %QPALM_DEPLOY_DIR%\lib
-	mkdir %QPALM_DEPLOY_DIR%\bin
-	mkdir %QPALM_DEPLOY_DIR%\include
+:: Create directories
+set QPALM_DEPLOY_DIR="qpalm-%QPALM_VERSION%-windows64"
 
-	:: powershell -NoExit -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))"
+mkdir %QPALM_DEPLOY_DIR%
+mkdir %QPALM_DEPLOY_DIR%\lib
+mkdir %QPALM_DEPLOY_DIR%\bin
+mkdir %QPALM_DEPLOY_DIR%\include
 
-	REM Copy License
-	xcopy ..\..\LICENSE %QPALM_DEPLOY_DIR%
-	REM Copy includes
-	xcopy ..\..\include\*.h %QPALM_DEPLOY_DIR%\include
-	REM Copy shared library
-	::powershell -NoExit -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))"
-	xcopy bin\Release\qpalm.dll %QPALM_DEPLOY_DIR%\bin
-	xcopy Release\qpalm.lib %QPALM_DEPLOY_DIR%\lib
+:: powershell -NoExit -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))"
 
-	REM Compress package
-	7z a -ttar %QPALM_DEPLOY_DIR%.tar %QPALM_DEPLOY_DIR%
-	7z a -tgzip %QPALM_DEPLOY_DIR%.tar.gz %QPALM_DEPLOY_DIR%.tar
+REM Copy License
+xcopy ..\..\LICENSE %QPALM_DEPLOY_DIR%
+REM Copy includes
+xcopy ..\..\include\*.h %QPALM_DEPLOY_DIR%\include
+REM Copy shared library
+::powershell -NoExit -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))"
+xcopy bin\Release\qpalm.dll %QPALM_DEPLOY_DIR%\bin
+xcopy Release\qpalm.lib %QPALM_DEPLOY_DIR%\lib
+
+REM Compress package
+7z a -ttar %QPALM_DEPLOY_DIR%.tar %QPALM_DEPLOY_DIR%
+7z a -tgzip %QPALM_DEPLOY_DIR%.tar.gz %QPALM_DEPLOY_DIR%.tar
 
 
-	:: Deploy package
-	curl -T %QPALM_DEPLOY_DIR%.tar.gz -ubenny44:%BINTRAY_API_KEY% -H "X-Bintray-Package:QPALM" -H "X-Bintray-Version:%QPALM_VERSION%" -H "X-Bintray-Override: 1" https://api.bintray.com/content/benny44/generic/QPALM/%QPALM_VERSION%/
+:: Deploy package
+curl -T %QPALM_DEPLOY_DIR%.tar.gz -ubenny44:%BINTRAY_API_KEY% -H "X-Bintray-Package:QPALM" -H "X-Bintray-Version:%QPALM_VERSION%" -H "X-Bintray-Override: 1" https://api.bintray.com/content/benny44/generic/QPALM/%QPALM_VERSION%/
 
-	if errorlevel 1 exit /b 1
+if errorlevel 1 exit /b 1
 
-	:: Publish deployed files
-	curl -X POST -ubenny44:%BINTRAY_API_KEY% https://api.bintray.com/content/benny44/generic/QPALM/%QPALM_VERSION%/publish
+:: Publish deployed files
+curl -X POST -ubenny44:%BINTRAY_API_KEY% https://api.bintray.com/content/benny44/generic/QPALM/%QPALM_VERSION%/publish
 
-	if errorlevel 1 exit /b 1
+if errorlevel 1 exit /b 1
 
-)
 
 @echo off
 
