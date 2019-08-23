176a6a44f42e2e736eeb70692c2ee8da1a435e1a
Ben Hermans <ben.hermans2@student.kuleuven.be>
2019-08-23 09:24:45 +0200

    Fix windows copy paths

--- appveyor/deploy.cmd
+++ appveyor/deploy.cmd
@@ -14,17 +14,18 @@ set QPALM_DEPLOY_DIR="qpalm-%QPALM_VERSION%-windows64"
 
 mkdir %QPALM_DEPLOY_DIR%
 mkdir %QPALM_DEPLOY_DIR%\lib
+mkdir %QPALM_DEPLOY_DIR%\bin
 mkdir %QPALM_DEPLOY_DIR%\include
 
-powershell -NoExit -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))"
+:: powershell -NoExit -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))"
 
 REM Copy License
 xcopy ..\..\LICENSE %QPALM_DEPLOY_DIR%
 REM Copy includes
 xcopy ..\..\include\*.h %QPALM_DEPLOY_DIR%\include
 REM Copy shared library
-xcopy bin\libqpalm.dll %QPALM_DEPLOY_DIR%\lib
-xcopy bin\libqpalm.lib %QPALM_DEPLOY_DIR%\lib
+xcopy bin\Release\libqpalm.dll %QPALM_DEPLOY_DIR%\bin
+xcopy Release\libqpalm.lib %QPALM_DEPLOY_DIR%\lib
 
 REM Compress package
 7z a -ttar %QPALM_DEPLOY_DIR%.tar %QPALM_DEPLOY_DIR%
